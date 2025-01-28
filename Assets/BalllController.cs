cf4341c76cb6d32810819542e12e411547d42756
EddyTian58 <bowentian58@gmail.com>
2025-01-28 01:16:47 -0800

    Added ball movement

--- Assets/BalllController.cs
+++ Assets/BalllController.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class BalllController : MonoBehaviour
 {
+    public Rigidbody sphereRigidbody;
+    public float ballSpeed = 2f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,24 +14,33 @@ public class BalllController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 inputVector = Vector2.zero;
         if(Input.GetKey(KeyCode.W))
-        { 
-            Debug.Log("User Input: W"); 
+        {
+            inputVector += Vector2.up;
+            Debug.Log("User Input: W");
         }
 
         if (Input.GetKey(KeyCode.S))
         {
+            inputVector += Vector2.down;
             Debug.Log("User Input: S");
         }
 
         if (Input.GetKey(KeyCode.A))
         {
+            inputVector += Vector2.left;
             Debug.Log("User Input: A");
         }
 
         if (Input.GetKey(KeyCode.D))
         {
+            inputVector += Vector2.right;
             Debug.Log("User Input: D");
         }
+        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
+        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+        Debug.Log("Resultant Vector: " + inputVector);
+        Debug.Log("Resultant 3D Vector: " + inputXZPlane);
     }
 }
