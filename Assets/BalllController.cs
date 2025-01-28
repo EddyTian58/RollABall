c42cf8db8a9c90324b4649a1e14b57e976b0ff4c
EddyTian58 <bowentian58@gmail.com>
2025-01-28 02:29:09 -0800

    Implemented jumping

--- Assets/BalllController.cs
+++ Assets/BalllController.cs
@@ -5,33 +5,23 @@ public class BalllController : MonoBehaviour
 {
     [SerializeField] private Rigidbody sphereRigidbody;
     [SerializeField] private float ballSpeed = 3f;
+    [SerializeField] private float jumpForce = 20f;
+    [SerializeField] private float gravityMuliplier = 0.5f;
     private Boolean onGround = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        Debug.Log("Calling the Start method");
-    }
     public void MoveBall(Vector3 input)
     {
-        Vector3 inputXYZPlane = new(input.x, input.y*10, input.z);
+        Vector3 inputXYZPlane = new(input.x, input.y*jumpForce, input.z);
         Debug.Log(input.y);
-        //if the ball jumps, set onGround to false
-        if (inputXYZPlane.y > 0)
-        {
-            onGround = false;
-        }
         
-        //if the ball isn't on the ground, keep it from jumping
         if (onGround == false)
         {
             inputXYZPlane.y = 0;
         }
         
-        Debug.Log(inputXYZPlane);
-        Debug.Log("onGround=" + onGround);
+        Debug.Log("Resultant vector:" + inputXYZPlane);
         sphereRigidbody.AddForce(inputXYZPlane * ballSpeed);
-        sphereRigidbody.AddForce(Physics.gravity * sphereRigidbody.mass);
+        sphereRigidbody.AddForce(Physics.gravity * (sphereRigidbody.mass*gravityMuliplier));
     }
 
     //detecting if the ball is on the ground
@@ -41,6 +31,16 @@ public class BalllController : MonoBehaviour
         {
             onGround = true;
             Debug.Log("ground touched");
+        }
+    }
+    
+    //detecting if the ball has left the ground
+    public void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            onGround = false;
+            Debug.Log("We have liftoff!");
         }
     }
 }
