3a2c7e65c1089f963053a7b09f3526e95dc7062b
EddyTian58 <bowentian58@gmail.com>
2025-01-28 04:12:09 -0800

    Implemented collision audio

--- Assets/BalllController.cs
+++ Assets/BalllController.cs
@@ -1,13 +1,23 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class BalllController : MonoBehaviour
 {
     [SerializeField] private Rigidbody sphereRigidbody;
+    [SerializeField] private AudioSource audioSource;
+    //I was trying to have some text that would display the number of times a wall was hit, but the text just would not change no matter what I tried
+    //[SerializeField] private TextMeshPro wallHitCountText;
     [SerializeField] private float ballSpeed = 3f;
     [SerializeField] private float jumpForce = 20f;
     [SerializeField] private float gravityMuliplier = 0.5f;
     private Boolean onGround = false;
+
+    //void Start()
+    //{
+    //    wallHitCountText = gameObject.GetComponent<TextMeshPro>();
+    //    wallHitCountText.SetText("Hello world?");
+    //}
 
     public void MoveBall(Vector3 input)
     {
@@ -24,13 +34,19 @@ public class BalllController : MonoBehaviour
         sphereRigidbody.AddForce(Physics.gravity * (sphereRigidbody.mass*gravityMuliplier));
     }
 
-    //detecting if the ball is on the ground
+    //detecting if the ball is on the ground or has hit a wall
     public void OnCollisionEnter(Collision collision)
     {
+        audioSource.time = 0.1f;
         if (collision.gameObject.tag == "Ground")
         {
             onGround = true;
+            audioSource.Play();
             Debug.Log("ground touched");
+        }
+        if (collision.gameObject.tag == "Wall")
+        {
+            audioSource.Play();
         }
     }
     
